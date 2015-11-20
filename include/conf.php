5fc97e04f62b09190ef36e6f1bf23975a47e4ec2
tectygroup <qycity@outlook.com>
2015-11-20 23:58:58 +0800

    Basic config of this project.

---
+++ include/conf.php
@@ -0,0 +1,22 @@
+<?php 
+//这里表示有没有经过配置，网页或者手动配置都需要改成1
+$conf=0;
+//这里是账户名称
+$UserName="";
+//这里是一个数组用于储存网盘的名字
+//多个值也是允许的，是array("DiskName1","DiskName2","DiskName3"等等)
+$DiskName=array("");
+//这里需要储存Form API的授权秘钥，可能是必要选项。
+$DiskFormToken=arry("");
+//防盗链的Token，每个空间一一对应的，不是必填选项。
+$DiskAuthorityToken=array("");
+
+
+
+
+
+
+
+
+
+?>
